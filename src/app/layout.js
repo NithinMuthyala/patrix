6a7d64cad8b18ddc5164ec72a8dfec349d7b92fc
NithinMuthyala <muthyala.nithin1996@gmail.com>
2025-04-05 11:34:05 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,26 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <link rel="stylesheet" href="assets/css/style.css" />
+        <link rel="stylesheet" href="assets/css/fontawesome.css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css" />
+        <script src="https://cdn.jsdelivr.net/gh/mcstudios/glightbox/dist/js/glightbox.min.js"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
         {children}
+        <script src="assets/vendors/js/glightbox.min.js"></script>
+
+        {/* <script type="text/javascript">
+          const lightbox = GLightbox({
+            'touchNavigation': true,
+          'href': 'https://www.youtube.com/watch?v=J9lS14nM1xg',
+          'type': 'video',
+          'source': 'youtube', //vimeo, youtube or local
+          'width': 900,
+          'autoPlayVideos': 'true',
+})
+
+        </script> */}
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
       </body>
     </html>
   );
